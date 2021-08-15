dfbe8ccf32cce76e6a0be5c7fa1061727e71eb45
nikita <nikita.grischin2015@yandex.ru>
2021-08-15 23:54:28 +0300

    added eslint and typescript

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-use-before-define
+import React from 'react';
+
+function App() {
+  return (
+    <div> working</div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-use-before-define
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);

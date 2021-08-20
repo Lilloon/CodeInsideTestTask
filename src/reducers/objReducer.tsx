58be94d531ad0781b0fc9f7259cd13dee7a58f37
nikita <nikita.grischin2015@yandex.ru>
2021-08-20 21:33:14 +0300

    reducer

--- src/reducers/objReducer.tsx
+++ src/reducers/objReducer.tsx
@@ -1,5 +1,16 @@
+import { OBJ_LIST_ACTIONS_TYPES } from '../actions/types'
+
 const defaultState = {
-  ObjList: [],
+  ObjList: [
+    {
+      id: 1,
+      objName: 'Математика',
+      numOf5: 5,
+      numOf4: 7,
+      numOf3: 3,
+      numOf2: 1,
+      missed: 5,
+    }],
 };
 
 export default (
@@ -7,6 +18,14 @@ export default (
   action:any,
 ) => {
   switch (action.type) {
+    case OBJ_LIST_ACTIONS_TYPES.ADD_OBJ:
+      return { ...state, ObjList: [...state.ObjList, action?.$payload] }
+    case OBJ_LIST_ACTIONS_TYPES.REMOVE_OBJ:
+      return {
+        ...state,
+        ObjList:
+        [...state.ObjList.filter((item) => (item.id !== action?.$payload))],
+      }
     default:
       return state;
   }

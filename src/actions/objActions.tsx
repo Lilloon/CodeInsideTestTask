696d645594bc3fa00cdc3dd5892cc8cbeb51212a
nikita <nikita.grischin2015@yandex.ru>
2021-08-27 19:25:24 +0300

    исправление роутера для gh pages

--- src/actions/objActions.tsx
+++
@@ -1,6 +0,0 @@
-import { createAction } from './actionCreator'
-import { OBJ_LIST_ACTIONS_TYPES } from './types'
-
-export const addObj = createAction(OBJ_LIST_ACTIONS_TYPES.ADD_OBJ)
-export const removeObj = createAction(OBJ_LIST_ACTIONS_TYPES.REMOVE_OBJ)
-export const setObjList = createAction(OBJ_LIST_ACTIONS_TYPES.SET_OBJ_LIST)
